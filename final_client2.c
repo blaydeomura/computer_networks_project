f4a5c1a12fbe7191bb1710391f97a55305a603e0
Blayde Omura <103175915+blaydeomura@users.noreply.github.com>
2023-11-20 14:09:17 -0800

    Pre Multi Thread

--- final_client2.c
+++ final_client2.c
@@ -38,6 +38,7 @@
 #include <errno.h>            
 #include "cJSON.c"
 #include "cJSON.h"
+#include <pthread.h>
 #define CONFIG_FILE "config.json"
 
 
@@ -321,240 +322,6 @@ allocate_intmem (int len) {
 }
 
 
-/*
-int
-main (int argc, char **argv) {
-
-  int i, status, sd, *ip_flags, *tcp_flags;
-  const int on = 1;
-  char *interface, *target, *src_ip, *dst_ip;
-  struct ip iphdr;
-  struct tcphdr tcphdr;
-  uint8_t *packet;
-  struct addrinfo hints, *res;
-  struct sockaddr_in *ipv4, sin;
-  struct ifreq ifr;
-  void *tmp;
-
-  // Allocate memory for various arrays.
-  packet = allocate_ustrmem (IP_MAXPACKET);
-  interface = allocate_strmem (40);
-  target = allocate_strmem (40);
-  src_ip = allocate_strmem (INET_ADDRSTRLEN);
-  dst_ip = allocate_strmem (INET_ADDRSTRLEN);
-  ip_flags = allocate_intmem (4);
-  tcp_flags = allocate_intmem (8);
-
-  // Interface to send packet through.
-  strcpy (interface, "enp0s1");
-
-  // Submit request for a socket descriptor to look up interface.
-  if ((sd = socket (AF_INET, SOCK_RAW, IPPROTO_RAW)) < 0) {
-    perror ("socket() failed to get socket descriptor for using ioctl() ");
-    exit (EXIT_FAILURE);
-  }
-
-  // Use ioctl() to look up interface index which we will use to
-  // bind socket descriptor sd to specified interface with setsockopt() since
-  // none of the other arguments of sendto() specify which interface to use.
-  memset (&ifr, 0, sizeof (ifr));
-  snprintf (ifr.ifr_name, sizeof (ifr.ifr_name), "%s", interface);
-  if (ioctl (sd, SIOCGIFINDEX, &ifr) < 0) {
-    perror ("ioctl() failed to find interface ");
-    return (EXIT_FAILURE);
-  }
-  close (sd);
-  printf ("Index for interface %s is %i\n", interface, ifr.ifr_ifindex);
-
-  // Source IPv4 address: you need to fill this out
-  strcpy (src_ip, "192.168.64.2");
-
-  // Destination URL or IPv4 address: you need to fill this out
-  strcpy (target, "192.168.64.3");
-
-  // Fill out hints for getaddrinfo().
-  memset (&hints, 0, sizeof (struct addrinfo));
-  hints.ai_family = AF_INET;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = hints.ai_flags | AI_CANONNAME;
-
-  // Resolve target using getaddrinfo().
-  if ((status = getaddrinfo (target, NULL, &hints, &res)) != 0) {
-    fprintf (stderr, "getaddrinfo() failed for target: %s\n", gai_strerror (status));
-    exit (EXIT_FAILURE);
-  }
-  ipv4 = (struct sockaddr_in *) res->ai_addr;
-  tmp = &(ipv4->sin_addr);
-  if (inet_ntop (AF_INET, tmp, dst_ip, INET_ADDRSTRLEN) == NULL) {
-    status = errno;
-    fprintf (stderr, "inet_ntop() failed for target.\nError message: %s", strerror (status));
-    exit (EXIT_FAILURE);
-  }
-  freeaddrinfo (res);
-
-  // IPv4 header
-
-  // IPv4 header length (4 bits): Number of 32-bit words in header = 5
-  iphdr.ip_hl = IP4_HDRLEN / sizeof (uint32_t);
-  // Internet Protocol version (4 bits): IPv4
-  iphdr.ip_v = 4;
-  // Type of service (8 bits)
-  iphdr.ip_tos = 0;
-  // Total length of datagram (16 bits): IP header + TCP header
-  iphdr.ip_len = htons (IP4_HDRLEN + TCP_HDRLEN);
-  // ID sequence number (16 bits): unused, since single datagram
-  iphdr.ip_id = htons (0);
-  // Flags, and Fragmentation offset (3, 13 bits): 0 since single datagram
-  // Zero (1 bit)
-  ip_flags[0] = 0;
-  // Do not fragment flag (1 bit)
-  ip_flags[1] = 0;
-  // More fragments following flag (1 bit)
-  ip_flags[2] = 0;
-  // Fragmentation offset (13 bits)
-  ip_flags[3] = 0;
-  iphdr.ip_off = htons ((ip_flags[0] << 15)
-                      + (ip_flags[1] << 14)
-                      + (ip_flags[2] << 13)
-                      +  ip_flags[3]);
-  // Time-to-Live (8 bits): default to maximum value
-  iphdr.ip_ttl = 255;
-  // Transport layer protocol (8 bits): 6 for TCP
-  iphdr.ip_p = IPPROTO_TCP;
-  // Source IPv4 address (32 bits)
-  if ((status = inet_pton (AF_INET, src_ip, &(iphdr.ip_src))) != 1) {
-    fprintf (stderr, "inet_pton() failed for source address.\nError message: %s", strerror (status));
-    exit (EXIT_FAILURE);
-  }
-
-  // Destination IPv4 address (32 bits)
-  if ((status = inet_pton (AF_INET, dst_ip, &(iphdr.ip_dst))) != 1) {
-    fprintf (stderr, "inet_pton() failed for destination address.\nError message: %s", strerror (status));
-    exit (EXIT_FAILURE);
-  }
-
-  // IPv4 header checksum (16 bits): set to 0 when calculating checksum
-  iphdr.ip_sum = 0;
-  iphdr.ip_sum = checksum ((uint16_t *) &iphdr, IP4_HDRLEN);
-
-  // TCP header
-
-  // Source port number (16 bits)
-  tcphdr.th_sport = htons (60);
-
-  // Destination port number (16 bits)
-  tcphdr.th_dport = htons (80);
-
-  // Sequence number (32 bits)
-  tcphdr.th_seq = htonl (0);
-
-  // Acknowledgement number (32 bits): 0 in first packet of SYN/ACK process
-  tcphdr.th_ack = htonl (0);
-
-  // Reserved (4 bits): should be 0
-  tcphdr.th_x2 = 0;
-
-  // Data offset (4 bits): size of TCP header in 32-bit words
-  tcphdr.th_off = TCP_HDRLEN / 4;
-
-  // Flags (8 bits)
-
-  // FIN flag (1 bit)
-  tcp_flags[0] = 0;
-
-  // SYN flag (1 bit): set to 1
-  tcp_flags[1] = 1;
-
-  // RST flag (1 bit)
-  tcp_flags[2] = 0;
-
-  // PSH flag (1 bit)
-  tcp_flags[3] = 0;
-
-  // ACK flag (1 bit)
-  tcp_flags[4] = 0;
-
-  // URG flag (1 bit)
-  tcp_flags[5] = 0;
-
-  // ECE flag (1 bit)
-  tcp_flags[6] = 0;
-
-  // CWR flag (1 bit)
-  tcp_flags[7] = 0;
-
-  tcphdr.th_flags = 0;
-  for (i=0; i<8; i++) {
-    tcphdr.th_flags += (tcp_flags[i] << i);
-  }
-
-  // Window size (16 bits)
-  tcphdr.th_win = htons (65535);
-
-  // Urgent pointer (16 bits): 0 (only valid if URG flag is set)
-  tcphdr.th_urp = htons (0);
-
-  // TCP checksum (16 bits)
-  tcphdr.th_sum = tcp4_checksum (iphdr, tcphdr);
-
-  // Prepare packet.
-
-  // First part is an IPv4 header.
-  memcpy (packet, &iphdr, IP4_HDRLEN * sizeof (uint8_t));
-
-  // Next part of packet is upper layer protocol header.
-  memcpy ((packet + IP4_HDRLEN), &tcphdr, TCP_HDRLEN * sizeof (uint8_t));
-
-  // The kernel is going to prepare layer 2 information (ethernet frame header) for us.
-  // For that, we need to specify a destination for the kernel in order for it
-  // to decide where to send the raw datagram. We fill in a struct in_addr with
-  // the desired destination IP address, and pass this structure to the sendto() function.
-  memset (&sin, 0, sizeof (struct sockaddr_in));
-  sin.sin_family = AF_INET;
-  sin.sin_addr.s_addr = iphdr.ip_dst.s_addr;
-
-  // Submit request for a raw socket descriptor.
-  if ((sd = socket (AF_INET, SOCK_RAW, IPPROTO_RAW)) < 0) {
-    perror ("socket() failed ");
-    exit (EXIT_FAILURE);
-  }
-
-  // Set flag so socket expects us to provide IPv4 header.
-  if (setsockopt (sd, IPPROTO_IP, IP_HDRINCL, &on, sizeof (on)) < 0) {
-    perror ("setsockopt() failed to set IP_HDRINCL ");
-    exit (EXIT_FAILURE);
-  }
-
-  // Bind socket to interface index.
-  if (setsockopt (sd, SOL_SOCKET, SO_BINDTODEVICE, &ifr, sizeof (ifr)) < 0) {
-    perror ("setsockopt() failed to bind to interface ");
-    exit (EXIT_FAILURE);
-  }
-
-  // Send packet.
-  if (sendto (sd, packet, IP4_HDRLEN + TCP_HDRLEN, 0, (struct sockaddr *) &sin, sizeof (struct sockaddr)) < 0)  {
-    perror ("sendto() failed ");
-    exit (EXIT_FAILURE);
-  }
-
-  // Close socket descriptor.
-  close (sd);
-
-  // Free allocated memory.
-  free (packet);
-  free (interface);
-  free (target);
-  free (src_ip);
-  free (dst_ip);
-  free (ip_flags);
-  free (tcp_flags);
-
-  return (EXIT_SUCCESS);
-}
-*/
-
-
-
 int setIPAndTCPHeaders(const struct ServerConfig* config, int destinationPort) {
 
   int i, status, sd, *ip_flags, *tcp_flags;
@@ -787,6 +554,31 @@ int setIPAndTCPHeaders(const struct ServerConfig* config, int destinationPort) {
 }
 
 
+//--------------UDP PACKET FUNCTION-----------------
+
+
+
+// Function to send SYN and UDP packets
+void* sendPackets(void* arg) {
+    struct ServerConfig* config = (struct ServerConfig*)arg;
+    setIPAndTCPHeaders(config, config->destinationPortTCPHeadSYN);
+
+    // TODO: Implement logic to send UDP packets here
+
+    setIPAndTCPHeaders(config, config->destinationPortTCPTailSYN);
+
+    pthread_exit(NULL);
+}
+
+
+// Function to listen for RST packets
+void* listenForRST(void* arg) {
+    // TODO: Implement logic to listen for RST packets here
+
+    pthread_exit(NULL);
+}
+
+
 int
 main (int argc, char **argv) {
     // Read the JSON configuration file
@@ -817,6 +609,45 @@ main (int argc, char **argv) {
     struct ServerConfig config = parseConfig(configData);
 
 
-    setIPAndTCPHeaders(&config, config.destinationPortTCPHeadSYN);
+   // setIPAndTCPHeaders(&config, config.destinationPortTCPHeadSYN);
+
+
+
+
+
+//------------atempting to multithread-------------
+    pthread_t sendThread, listenThread;
+
+    // Create threads
+    if (pthread_create(&sendThread, NULL, sendPackets, (void*)&config) != 0) {
+        perror("Error creating send thread");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pthread_create(&listenThread, NULL, listenForRST, NULL) != 0) {
+        perror("Error creating listen thread");
+        exit(EXIT_FAILURE);
+    }
+
+    // Wait for threads to finish
+    if (pthread_join(sendThread, NULL) != 0) {
+        perror("Error joining send thread");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pthread_join(listenThread, NULL) != 0) {
+        perror("Error joining listen thread");
+        exit(EXIT_FAILURE);
+    }
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
